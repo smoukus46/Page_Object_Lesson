d59b181fa4cc6d00e8f2d5d12419e03f37bfe783
unknown <smoukus@mail.ru>
2023-07-17 22:10:58 +0300

    Добавил переход

--- test_main_page.py
+++ test_main_page.py
@@ -4,18 +4,20 @@ from .pages.login_page import LoginPage
 from selenium.common.exceptions import NoSuchElementException
 
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/accounts/login/"
+    link = "http://selenium1py.pythonanywhere.com"
     page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
     page.open()                      # открываем страницу
-    page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
 
 def test_guest_should_see_login_link(browser):
-    link = "http://selenium1py.pythonanywhere.com/accounts/login/"
+    link = "http://selenium1py.pythonanywhere.com"
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
 
-def test_login_page(browser):
+def test_login_page_form(browser):
     link = "http://selenium1py.pythonanywhere.com/accounts/login/"
     page = MainPage(browser, link)
     page.open()
